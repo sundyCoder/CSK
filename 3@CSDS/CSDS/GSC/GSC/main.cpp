e1d88cf1b535f19a93e324f98021589b9ba4edf7
sundy <sundycoder@gmail.com>
2017-11-08 19:03:59 +0800

    greedy algorithm.

--- 3@CSDS/CSDS/GSC/GSC/main.cpp
+++ 3@CSDS/CSDS/GSC/GSC/main.cpp
@@ -6,9 +6,9 @@
 using namespace std;
 using namespace sort;
 
-int main(int argc, char** argv) {
+void sort_test() {
 	//int A[] = {31,41,59,26,41,58};
-	int A[] = {2,5,3,0,2,3,0,3};
+	int A[] = { 2,5,3,0,2,3,0,3 };
 	int len = sizeof(A) / sizeof(int);
 	Solution<int> solu;
 	//int ret = solu.insertionSort(A,len);		
@@ -22,18 +22,25 @@ int main(int argc, char** argv) {
 
 	//int ret = solu.mergeSort(A, 0, len -1);   
 
-	//int ret = solu.radixSort(A, len);
 	auto start = std::chrono::system_clock::now();
-	int ret = solu.countSort(A, len);
+	//int ret = solu.countSort(A, len);
+	//int ret = solu.radixSort(A, len);
+	float Af[] = { 0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434 };
+	len = sizeof(Af) / sizeof(int);
+	int ret = solu.bucketSort(Af, len);
 	auto end = std::chrono::system_clock::now();
-	std::cout << "It takes " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()<<"us.\n";
-	//int ret = solu.bucketSort(A, len);
+	std::cout << "It takes " << std::chrono::duration_cast
+		<std::chrono::microseconds>(end - start).count() << "us.\n";
 	if (ret) {
 		for (int i = 0; i < len; i++) {
-			std::cout << A[i] << " ";
+			std::cout << Af[i] << " ";
 		}
 		std::cout << std::endl;
 	}
+}
+
+int main(int argc, char** argv) {
+	sort_test();
 	system("pause");
 	return 0;
 }
