7458dee715bb46a40ab44dc2c66ea87fd63e7675
sundy <sundycoder@gmail.com>
2017-11-12 15:09:24 +0800

    update

--- 3@CSDS/CSDS/GSC/GSC/tree/bt.hpp
+++ 3@CSDS/CSDS/GSC/GSC/tree/bt.hpp
@@ -4,55 +4,69 @@
 using namespace std;
 
 namespace bitree {
+	template <typename Dtype>
+	class BItree {
 	/*
 	* Definition for a binary tree node.
 	*/
-	template <typename Dtype>
 	struct TreeNode {
-		Dtype val;
+		int val;
 		TreeNode *left;
 		TreeNode *right;
-		TreeNode(Dtype x) : val(x), left(NULL), right(NULL) {}
+		TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 	};
 
+	TreeNode* root;
 
-	template <typename Dtype>
-	class BItree {
+	void bt_insert(TreeNode*& root, TreeNode* n);
+	void bt_insert(TreeNode*& root, Dtype e);
+	vector<Dtype> bt_travelRe(TreeNode*& root);
+	vector<Dtype> bt_travelIt(TreeNode*& root);
+
 	public:
-		BItree() :root(NULL) {}
-		struct TreeNode* newNode(Dtype e);
+		BItree() :root(NULL) {};
+		void btTreeInsert(Dtype e) { bt_insert(root, e); };
+		vector<Dtype> btTreeTravelRe() { return bt_travelRe(root); };
+		vector<Dtype> btTreeTravelIt() { return bt_travelIt(root); };
 
-		vector<int> preorderTraversalRe(TreeNode* root);
-		vector<int> preorderTraversalIt(TreeNode* root);
 	};
 
 	template <typename Dtype>
-	struct TreeNode* BItree<Dtype>::newNode(Dtype e) {
-		struct TreeNode* node = (struct TreeNode*) malloc(sizeof(struct TreeNode));
-		node->val= e;
-		node->left = NULL;
-		node->right = NULL;
+	void BItree<Dtype>::bt_insert(TreeNode*& root, TreeNode* n) {
+		if (root == NULL) {
+			root = n;
+			return;
+		}
 
-		return(node);
+		if (n->val < root->val) {
+			bt_insert(root->left, n);
+		} else {
+			bt_insert(root->right, n);
+		}
+	}
+
+	template <typename Dtype>
+	void BItree<Dtype>::bt_insert(TreeNode*& root, Dtype e) {
+		bt_insert(root, new TreeNode(e));
 	}
 
 	//1 preorder
 	// 1.1 solution using recursive
 	template <typename Dtype>
-	vector<Dtype> BItree<Dtype>::preorderTraversalRe(TreeNode* root) {
+	vector<Dtype> BItree<Dtype>::bt_travelRe(TreeNode*& root) {
 		if (root == NULL) {
 			return vector<Dtype>();
 		}
 
 		vector<Dtype> vecTree;
 		vecTree.push_back(root->val);
-		preorderTraversalRe(root->left);
-		preorderTraversalRe(root->right);
+		bt_travelRe(root->left);
+		bt_travelRe(root->right);
 	}
 
 	// 1.2 solution using stack 
 	template <typename Dtype>
-	vector<int> BItree<Dtype>::preorderTraversalIt(TreeNode* root) {
-
+	vector<Dtype> BItree<Dtype>::bt_travelIt(TreeNode*& root) {
+		return vector<Dtype>();
 	}
 }
